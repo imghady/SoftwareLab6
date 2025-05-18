d2472bd9187e453f72346e05f56fb1e0b03a92d7
imghady <amirreza8010@gmail.com>
2025-05-18 22:19:39 +0330

    add dockerfile and docker-compose.yml files

--- notes/settings.py
+++ notes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,14 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", 'mydatabase'),
+        'USER': os.getenv("DB_USER", 'mydatabaseuser'),
+        'PASSWORD': os.getenv("DB_PASSWORD", 'mypassword'),
+        'HOST': os.getenv("DB_HOST", '127.0.0.1'),
+        'PORT': os.getenv("DB_PORT", '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
